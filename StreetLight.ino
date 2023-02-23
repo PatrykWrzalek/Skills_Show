fb0f35db8d9a0eef5720f9ed82ca125ddc3c27dd
Patryk Wrzalek <wrzalek.patryk@gmail.com>
2023-02-23 12:12:36 +0100

    Adding a crosswalk buttons to the program

--- StreetLight.ino
+++ StreetLight.ino
@@ -2,59 +2,88 @@
 #define MHz 1000000
 #define kHz 1000*/
 #define LightNumber 3
+#define ButtonNumber 1
+#define LightStartsFromPin  0
+#define ButtonStartsFromPin 8
 
 //uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
-uint8_t LightPin[LightNumber];
+uint8_t LightPin[LightNumber], ButtonPin[ButtonNumber];
+uint8_t (*GreenLightStreet), (*YellowLightStreet), (*RedLightStreet);
 
-void streetlight_first_app()        /*First program*/
+void init_LED_ladder()             //Initialization LED_ladder programs
+{
+    pinMode(LED_BUILTIN, OUTPUT); 
+    for (uint8_t i = 0; i < ButtonNumber; i++)
+        {
+        ButtonPin[i]=i+ButtonStartsFromPin;
+        pinMode(ButtonPin[i], INPUT_PULLUP); 
+        }
+    for (uint8_t i = 0; i < LightNumber; i++)
+        {
+        LightPin[i]=i+LightStartsFromPin;
+        pinMode(LightPin[i], OUTPUT); 
+        digitalWrite(LightPin[i], LOW);
+        }
+}
+void LED_3ladder_first_app()       //First function
 {
     digitalWrite(LightPin[2], HIGH);
     digitalWrite(LightPin[1], LOW);
     digitalWrite(LightPin[0], LOW);
-    digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
     digitalWrite(LightPin[2], LOW);
     digitalWrite(LightPin[1], HIGH);
     digitalWrite(LightPin[0], LOW);
-    digitalWrite(LED_BUILTIN, HIGH);
     delay(1000);
     digitalWrite(LightPin[2], LOW);
     digitalWrite(LightPin[1], LOW);
     digitalWrite(LightPin[0], HIGH);
-    digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
 }
-void streetlight_second_app()       /*Second program*/
+void LED_ladder_second_app()       //Second function
 {
-for (uint8_t i = 0; i < LightNumber; i++)
-    {
-        if(i==0) 
+    for (uint8_t i = 0; i < LightNumber; i++)
         {
+        if(i==0) 
+            {
             digitalWrite(LightPin[LightNumber-1], LOW);
             digitalWrite(LightPin[i], HIGH);
-        }
+            }
         else
-        {
+            {
             digitalWrite(LightPin[i], HIGH);
             digitalWrite(LightPin[i-1], LOW);
-        }
-        digitalWrite(LED_BUILTIN, bool(i/2));
+            }
         delay(1000);
-    }
+        }
+}
+void streetlight_init ()           //Assigning meaning to the streetlight_app
+{
+    GreenLightStreet=&LightPin[2];        
+    YellowLightStreet=&LightPin[1];
+    RedLightStreet=&LightPin[0];
+}
+void streetlight_app(bool crosswalk_open)   //Streetlight_app (street lights control)
+{
+    if(crosswalk_open==HIGH) 
+        {
+        digitalWrite((*RedLightStreet), LOW);
+        digitalWrite((*GreenLightStreet), HIGH);
+        }
+    else 
+        {
+        digitalWrite((*RedLightStreet), HIGH);
+        digitalWrite((*GreenLightStreet), LOW);
+        }
 }
 
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT); 
-    for (uint8_t i = 0; i < LightNumber; i++)
-    {
-        LightPin[i]=i;
-        pinMode(LightPin[i], OUTPUT); 
-        //digitalWrite(LightPin[i], LOW);
-    }
+    init_LED_ladder();
+    streetlight_init();
 }
 
 void loop()
 {
-    streetlight_second_app();
+    streetlight_app(digitalRead(ButtonPin[0]));
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -4,58 +4,87 @@
 #define MHz 1000000
 
 #define kHz 1000*/
-//uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
-uint8_t LightPin[3];
 
-void streetlight_first_app() /*First program*/
+
+
+//uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
+uint8_t LightPin[3], ButtonPin[1];
+uint8_t (*GreenLightStreet), (*YellowLightStreet), (*RedLightStreet);
+
+void init_LED_ladder() //Initialization LED_ladder programs
+{
+    pinMode(13, 0x1);
+    for (uint8_t i = 0; i < 1; i++)
+        {
+        ButtonPin[i]=i+8;
+        pinMode(ButtonPin[i], 0x2);
+        }
+    for (uint8_t i = 0; i < 3; i++)
+        {
+        LightPin[i]=i+0;
+        pinMode(LightPin[i], 0x1);
+        digitalWrite(LightPin[i], 0x0);
+        }
+}
+void LED_3ladder_first_app() //First program
 {
     digitalWrite(LightPin[2], 0x1);
     digitalWrite(LightPin[1], 0x0);
     digitalWrite(LightPin[0], 0x0);
-    digitalWrite(13, 0x0);
     delay(1000);
     digitalWrite(LightPin[2], 0x0);
     digitalWrite(LightPin[1], 0x1);
     digitalWrite(LightPin[0], 0x0);
-    digitalWrite(13, 0x1);
     delay(1000);
     digitalWrite(LightPin[2], 0x0);
     digitalWrite(LightPin[1], 0x0);
     digitalWrite(LightPin[0], 0x1);
-    digitalWrite(13, 0x0);
     delay(1000);
 }
-void streetlight_second_app() /*Second program*/
+void LED_ladder_second_app() //Second program
 {
-for (uint8_t i = 0; i < 3; i++)
-    {
-        if(i==0)
+    for (uint8_t i = 0; i < 3; i++)
         {
+        if(i==0)
+            {
             digitalWrite(LightPin[3 -1], 0x0);
             digitalWrite(LightPin[i], 0x1);
-        }
+            }
         else
-        {
+            {
             digitalWrite(LightPin[i], 0x1);
             digitalWrite(LightPin[i-1], 0x0);
-        }
-        digitalWrite(13, bool(i/2));
+            }
         delay(1000);
-    }
+        }
+}
+void streetlight_init () //Assigning meaning to the streetlight_app
+{
+    GreenLightStreet=&LightPin[2];
+    YellowLightStreet=&LightPin[1];
+    RedLightStreet=&LightPin[0];
+}
+void streetlight_app(bool crosswalk_open) //Streetlight_app (street lights control)
+{
+    if(crosswalk_open==0x1)
+        {
+        digitalWrite((*RedLightStreet), 0x0);
+        digitalWrite((*GreenLightStreet), 0x1);
+        }
+    else
+        {
+        digitalWrite((*RedLightStreet), 0x1);
+        digitalWrite((*GreenLightStreet), 0x0);
+        }
 }
 
 void setup()
 {
-    pinMode(13, 0x1);
-    for (uint8_t i = 0; i < 3; i++)
-    {
-        LightPin[i]=i;
-        pinMode(LightPin[i], 0x1);
-        //digitalWrite(LightPin[i], LOW);
-    }
+    init_LED_ladder();
+    streetlight_init();
 }
 
 void loop()
 {
-    streetlight_second_app();
+    streetlight_app(digitalRead(ButtonPin[0]));
 }
