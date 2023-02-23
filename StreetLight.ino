d9664d03a812ae4018ff2b488481e55c593aa44c
Patryk Wrzalek <wrzalek.patryk@gmail.com>
2023-02-23 13:19:30 +0100

    Adding function is_window_open and UART_init

--- StreetLight.ino
+++ StreetLight.ino
@@ -1,14 +1,16 @@
 /*#define GHz 1000000000
 #define MHz 1000000
 #define kHz 1000*/
+//#include <XH_ConnectorTester.ino>
 #define LightNumber 3
 #define ButtonNumber 1
-#define LightStartsFromPin  0
+#define LightStartsFromPin  5
 #define ButtonStartsFromPin 8
 
 //uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
 uint8_t LightPin[LightNumber], ButtonPin[ButtonNumber];
 uint8_t (*GreenLightStreet), (*YellowLightStreet), (*RedLightStreet);
+uint8_t (*GreenLightWindow), (*RedLightWindow);
 
 void init_LED_ladder()             //Initialization LED_ladder programs
 {
@@ -57,33 +59,58 @@ void LED_ladder_second_app()       //Second function
         delay(1000);
         }
 }
+
 void streetlight_init ()           //Assigning meaning to the streetlight_app
 {
     GreenLightStreet=&LightPin[2];        
     YellowLightStreet=&LightPin[1];
     RedLightStreet=&LightPin[0];
 }
-void streetlight_app(bool crosswalk_open)   //Streetlight_app (street lights control)
+void streetlight_app()   //Streetlight_app (street lights control)
 {
-    if(crosswalk_open==HIGH) 
-        {
-        digitalWrite((*RedLightStreet), LOW);
-        digitalWrite((*GreenLightStreet), HIGH);
-        }
-    else 
-        {
-        digitalWrite((*RedLightStreet), HIGH);
-        digitalWrite((*GreenLightStreet), LOW);
-        }
+    
 }
 
 void setup()
 {
     init_LED_ladder();
-    streetlight_init();
+    windowlight_init();
+    UART_init();
 }
 
 void loop()
 {
-    streetlight_app(digitalRead(ButtonPin[0]));
+    is_window_open(digitalRead(ButtonPin[0]));
+    wait_till_next_action_window();
+}
+/////////////////////////////////////////////
+void UART_init()        //Initialization UART
+{
+    Serial.begin(115200);
+    Serial.println("Hello Unknown :D");
+}
+void windowlight_init ()           //Assigning meaning to the is_window_open
+{
+    GreenLightWindow=&LightPin[2];        
+    RedLightWindow=&LightPin[0];
+}
+void is_window_open(bool window_status)
+{
+    if(window_status==HIGH) 
+        {
+        digitalWrite((*RedLightWindow), LOW);
+        digitalWrite((*GreenLightWindow), HIGH);
+        Serial.println("Window is close!");
+        }
+    else 
+        {
+        digitalWrite((*RedLightWindow), HIGH);
+        digitalWrite((*GreenLightWindow), LOW);
+        Serial.println("Alarm! Window is open!");
+        }
+}
+void wait_till_next_action_window()
+{
+    if (digitalRead(ButtonPin[0])==HIGH) while (digitalRead(ButtonPin[0])==HIGH) delay(5);
+    else if (digitalRead(ButtonPin[0])==LOW) while (digitalRead(ButtonPin[0])==LOW) delay(5);
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -4,12 +4,17 @@
 #define MHz 1000000
 
 #define kHz 1000*/
+# 4 "c:\\Users\\Patryk\\Desktop\\App\\Arduino XH-Conector_Tester\\StreetLight.ino"
+//#include <XH_ConnectorTester.ino>
+
+
 
 
 
 //uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
 uint8_t LightPin[3], ButtonPin[1];
 uint8_t (*GreenLightStreet), (*YellowLightStreet), (*RedLightStreet);
+uint8_t (*GreenLightWindow), (*RedLightWindow);
 
 void init_LED_ladder() //Initialization LED_ladder programs
 {
@@ -21,12 +26,12 @@ void init_LED_ladder() //Initialization LED_ladder programs
         }
     for (uint8_t i = 0; i < 3; i++)
         {
-        LightPin[i]=i+0;
+        LightPin[i]=i+5;
         pinMode(LightPin[i], 0x1);
         digitalWrite(LightPin[i], 0x0);
         }
 }
-void LED_3ladder_first_app() //First program
+void LED_3ladder_first_app() //First function
 {
     digitalWrite(LightPin[2], 0x1);
     digitalWrite(LightPin[1], 0x0);
@@ -41,7 +46,7 @@ void LED_3ladder_first_app() //First program
     digitalWrite(LightPin[0], 0x1);
     delay(1000);
 }
-void LED_ladder_second_app() //Second program
+void LED_ladder_second_app() //Second function
 {
     for (uint8_t i = 0; i < 3; i++)
         {
@@ -58,33 +63,58 @@ void LED_ladder_second_app() //Second program
         delay(1000);
         }
 }
+
 void streetlight_init () //Assigning meaning to the streetlight_app
 {
     GreenLightStreet=&LightPin[2];
     YellowLightStreet=&LightPin[1];
     RedLightStreet=&LightPin[0];
 }
-void streetlight_app(bool crosswalk_open) //Streetlight_app (street lights control)
+void streetlight_app() //Streetlight_app (street lights control)
 {
-    if(crosswalk_open==0x1)
-        {
-        digitalWrite((*RedLightStreet), 0x0);
-        digitalWrite((*GreenLightStreet), 0x1);
-        }
-    else
-        {
-        digitalWrite((*RedLightStreet), 0x1);
-        digitalWrite((*GreenLightStreet), 0x0);
-        }
+
 }
 
 void setup()
 {
     init_LED_ladder();
-    streetlight_init();
+    windowlight_init();
+    UART_init();
 }
 
 void loop()
 {
-    streetlight_app(digitalRead(ButtonPin[0]));
+    is_window_open(digitalRead(ButtonPin[0]));
+    wait_till_next_action_window();
+}
+/////////////////////////////////////////////
+void UART_init() //Initialization UART
+{
+    Serial.begin(115200);
+    Serial.println("Hello Unknown :D");
+}
+void windowlight_init () //Assigning meaning to the is_window_open
+{
+    GreenLightWindow=&LightPin[2];
+    RedLightWindow=&LightPin[0];
+}
+void is_window_open(bool window_status)
+{
+    if(window_status==0x1)
+        {
+        digitalWrite((*RedLightWindow), 0x0);
+        digitalWrite((*GreenLightWindow), 0x1);
+        Serial.println("Window is close!");
+        }
+    else
+        {
+        digitalWrite((*RedLightWindow), 0x1);
+        digitalWrite((*GreenLightWindow), 0x0);
+        Serial.println("Alarm! Window is open!");
+        }
+}
+void wait_till_next_action_window()
+{
+    if (digitalRead(ButtonPin[0])==0x1) while (digitalRead(ButtonPin[0])==0x1) delay(5);
+    else if (digitalRead(ButtonPin[0])==0x0) while (digitalRead(ButtonPin[0])==0x0) delay(5);
 }
