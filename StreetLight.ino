3706c5b2c44343f0cb4c29c5b368cfe142138e5c
Patryk Wrzalek <wrzalek.patryk@gmail.com>
2023-02-23 10:13:00 +0100

    Adding second function to control street light

--- StreetLight.ino
+++ StreetLight.ino
@@ -6,18 +6,7 @@
 //uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
 uint8_t LightPin[LightNumber];
 
-void setup()
-{
-    pinMode(LED_BUILTIN, OUTPUT); 
-    for (uint8_t i = 0; i < LightNumber; i++)
-    {
-        LightPin[i]=i;
-        pinMode(LightPin[i], OUTPUT); 
-        //digitalWrite(LightPin[i], LOW);
-    }
-}
-
-void loop()
+void streetlight_first_app()        /*First program*/
 {
     digitalWrite(LightPin[2], HIGH);
     digitalWrite(LightPin[1], LOW);
@@ -34,4 +23,38 @@ void loop()
     digitalWrite(LightPin[0], HIGH);
     digitalWrite(LED_BUILTIN, LOW);
     delay(1000);
+}
+void streetlight_second_app()       /*Second program*/
+{
+for (uint8_t i = 0; i < LightNumber; i++)
+    {
+        if(i==0) 
+        {
+            digitalWrite(LightPin[LightNumber-1], LOW);
+            digitalWrite(LightPin[i], HIGH);
+        }
+        else
+        {
+            digitalWrite(LightPin[i], HIGH);
+            digitalWrite(LightPin[i-1], LOW);
+        }
+        digitalWrite(LED_BUILTIN, bool(i/2));
+        delay(1000);
+    }
+}
+
+void setup()
+{
+    pinMode(LED_BUILTIN, OUTPUT); 
+    for (uint8_t i = 0; i < LightNumber; i++)
+    {
+        LightPin[i]=i;
+        pinMode(LightPin[i], OUTPUT); 
+        //digitalWrite(LightPin[i], LOW);
+    }
+}
+
+void loop()
+{
+    streetlight_second_app();
 }
--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -7,18 +7,7 @@
 //uint32_t MCU_Freguancy = 16 * MHz; //Max MCU_Freguancy 4 294 967 296 Hz
 uint8_t LightPin[3];
 
-void setup()
-{
-    pinMode(13, 0x1);
-    for (uint8_t i = 0; i < 3; i++)
-    {
-        LightPin[i]=i;
-        pinMode(LightPin[i], 0x1);
-        //digitalWrite(LightPin[i], LOW);
-    }
-}
-
-void loop()
+void streetlight_first_app() /*First program*/
 {
     digitalWrite(LightPin[2], 0x1);
     digitalWrite(LightPin[1], 0x0);
@@ -35,4 +24,38 @@ void loop()
     digitalWrite(LightPin[0], 0x1);
     digitalWrite(13, 0x0);
     delay(1000);
+}
+void streetlight_second_app() /*Second program*/
+{
+for (uint8_t i = 0; i < 3; i++)
+    {
+        if(i==0)
+        {
+            digitalWrite(LightPin[3 -1], 0x0);
+            digitalWrite(LightPin[i], 0x1);
+        }
+        else
+        {
+            digitalWrite(LightPin[i], 0x1);
+            digitalWrite(LightPin[i-1], 0x0);
+        }
+        digitalWrite(13, bool(i/2));
+        delay(1000);
+    }
+}
+
+void setup()
+{
+    pinMode(13, 0x1);
+    for (uint8_t i = 0; i < 3; i++)
+    {
+        LightPin[i]=i;
+        pinMode(LightPin[i], 0x1);
+        //digitalWrite(LightPin[i], LOW);
+    }
+}
+
+void loop()
+{
+    streetlight_second_app();
 }
